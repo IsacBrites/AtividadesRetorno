3b907e30e04ca9dd45f6bd018536f60597f36c1e
Isac <britesisac@gmail.com>
2026-08-21 15:00:20 -0300

    atualização

--- src/CorretoraDeImoveis/Imoboliaria.java
+++ src/CorretoraDeImoveis/Imoboliaria.java
@@ -1,5 +1,6 @@
 package CorretoraDeImoveis;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Imoboliaria {
@@ -18,6 +19,34 @@ public class Imoboliaria {
         this.propostas = new ArrayList<>();
     }
 
+    public Imovel buscarImovel (String codigo){
+        for (Imovel imovel : imoveis){
+            if (imovel.getCodigo().equals(codigo)){
+                return imovel;
+            }
+        }
+        return null;
+    }
+
+    public Cliente buscarCliente(String cpf){
+        for (Cliente cliente : clientes){
+            if (cliente.getCpf().equals(cpf)){
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    public Corretor buscarCorretor(String crei){
+        for (Corretor corretor : corretores){
+            if (corretor.getCreci().equals(crei)){
+                return corretor;
+            }
+        }
+        return null;
+    }
+
+
     public void cadastrarImovel(Imovel imovel){
         this.imoveis.add(imovel);
     }
@@ -26,6 +55,13 @@ public class Imoboliaria {
     }
     public void cadastrarCliente(Cliente cliente){
         this.clientes.add(cliente);
+    }
+
+    public void agendarVisita(String codigoImovel, LocalDate data){
+        Imovel imovel = buscarImovel(codigoImovel);
+        if (imovel != null){
+            imovel.registrarVisistas(data);
+        }
     }
 
 }
